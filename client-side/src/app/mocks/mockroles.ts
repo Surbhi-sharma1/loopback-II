61d862be5c31aca7c0a33481532d045ea6b1afce
surbhi-sharma1 <surbhi.sharma1@sourcefuse.com>
2022-07-26 11:38:03 +0530

    s

--- client-side/src/app/mocks/mockroles.ts
+++ client-side/src/app/mocks/mockroles.ts
@@ -1,25 +1,25 @@
-import { role } from "../userModel";
+import { role } from '../userModel'
 
 export const mockRoles: role[] = [
-    {
-        "key": "superadmin",
-        "name": "Super Admin",
-        "description": "Supervises admin and his activities."
-    },
-    {
-        "key": "admin",
-        "name": "Admin",
-        "description": "Manages all work."
-    },
-    {
-        "key": "subscriber",
-        "name": "Subscriber",
-        "description": "Subscribed for services."
-    }
-];
+  {
+    key: 'superadmin',
+    name: 'Super Admin',
+    description: 'Supervis admin and his activities.',
+  },
+  {
+    key: 'admin',
+    name: 'Admin',
+    description: 'Manages all work.',
+  },
+  {
+    key: 'subscriber',
+    name: 'Subscriber',
+    description: 'Subscribed for services.',
+  },
+]
 
 export const mockRoleByKey = {
-    "key": "admin",
-    "name": "Admin",
-    "description": "Manages all work."
-};
+  key: 'admin',
+  name: 'Admin',
+  description: 'Manages all work.',
+}
